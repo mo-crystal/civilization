30c4259174dac5bb6cf95a09d8742df5639661df
Edmond-J-A <edomondja@gmail.com>
2023-07-06 00:27:02 +0800

    add creature struct

---
+++ src/core/creature.cc
@@ -0,0 +1,14 @@
+#include "creature.h"
+
+Creature::Creature()
+{
+}
+
+Creature::Creature(Point _location)
+{
+  this->location = _location;
+}
+
+Creature::~Creature()
+{
+}
---
+++ src/core/creature.h
@@ -0,0 +1,12 @@
+#include "point.h"
+class Creature
+{
+private:
+  Point location;
+  int health = 100;
+  int speed = 0;
+public:
+  Creature();
+  Creature(Point _location);
+  ~Creature();
+};
