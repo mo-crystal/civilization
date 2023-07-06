770a650857167b35ec265a1f8faebcc9f4afb52e
Edmond-J-A <edomondja@gmail.com>
2023-07-07 00:12:48 +0800

    add operator for item

--- src/core/item.cc
+++ src/core/item.cc
@@ -1,10 +1,11 @@
-#include"item.h" 
+#include "item.h"
 
-Item::Item(int _ID,std::string _name,int _maxStack)
+Item::Item(int _ID, std::string _name, int _maxStack, int _nowStack = 1)
 {
-  this->ID=ID;
-  this->name=_name;
-  this->maxStack=_maxStack;
+  this->ID = _ID;
+  this->name = _name;
+  this->maxStack = _maxStack;
+  this->nowStack = _nowStack;
 }
 
 Item::~Item()
--- src/core/item.h
+++ src/core/item.h
@@ -3,14 +3,23 @@
 class Item
 {
 private:
-  int ID;
-  std::string name;
+  int ID = -1;
+  std::string name = "";
   int maxStack = 1;
   int nowStack = 1;
   double Durability = -1;
 
 public:
-  Item(int _ID, std::string _name, int _maxStack);
+  Item();
+  Item(int _ID, std::string _name, int _maxStack, int _nowStack = 1);
   ~Item();
-  bool IsMax() { return maxStack == nowStack; }
+  int GetID() { return this->ID; }
+  std::string GetName() { return this->name; }
+  int GetNowStack() { return this->nowStack; }
+  int GetMax() { return this->maxStack; }
+  void SetDurability(double d) { this->Durability = d; }
+  double GetDurability() { return this->Durability; }
+  bool IsMax() { return maxStack >= nowStack; }
+  bool operator==(const Item &other) { return (this->ID == other.ID && this->name == other.name); }
+  bool IsValid() { return ID > 0; }
 };
