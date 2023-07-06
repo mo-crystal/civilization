5483f6d41fe71e0437f254b8508579c3073fe337
Edmond-J-A <edomondja@gmail.com>
2023-07-07 00:13:56 +0800

    add id in player

--- src/core/player.cc
+++ src/core/player.cc
@@ -1,9 +1,11 @@
 #include "player.h"
 
-Player::Player(Point _location) : Creature(_location)
+Player::Player(int _id, Point _location) : Creature(_location)
 {
+  this->ID = _id;
 }
 
 Player::~Player()
 {
 }
+
