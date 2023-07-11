1dd02e0e228286c2ca8ee1eb33861177a1ae2e31
Edmond-J-A <edomondja@gmail.com>
2023-07-12 00:36:14 +0800

    add setownerid

--- src/core/construct.h
+++ src/core/construct.h
@@ -3,11 +3,13 @@ class Construct
 private:
   int x, y;
   bool blockable = false;
+  int ownerID = 0;
 
 public:
   Construct();
-  Construct(int _x, int _y,bool _blockable);
+  Construct(int _x, int _y, bool _blockable);
   ~Construct();
+  bool SetOwnerID(int ID);
   virtual bool TickAction() = 0;
   virtual bool Place() = 0;
   virtual bool Destory() = 0;
