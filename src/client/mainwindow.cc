38449f3d5d893f42b7d7af567c10228b3d811ce3
Edmond-J-A <edomondja@gmail.com>
2023-07-14 22:46:45 +0800

    remove game class

--- src/client/mainwindow.cc
+++ src/client/mainwindow.cc
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
   ui->setupUi(this);
   ui->stackedWidget->setStyleSheet("background-color: transparent;");
-  g.SetMapRange(BLOCK_SIZE);
+  Init();
 
   pressTimer = new QTimer(this);
   pressTimer->setSingleShot(true);
@@ -34,12 +34,9 @@ MainWindow::MainWindow(QWidget *parent)
   ui->setting_movie->setMovie(movie);
   movie->start();
 
-  // 引擎初始化
-  std::map<Point, int> a = this->g.DecorateInit(BLOCK_SIZE);
-
   // 玩家初始化（目前不正规）
   Player *p = new Player(1, Point(MAP_WIDTH * BLOCK_SIZE / 2 - 32, MAP_HEIGHT * BLOCK_SIZE / 2 - 32));
-  this->g.SetPlayer(p);
+  this->SetPlayer(p);
   Animation this_player;
   this_player.SetTimerID(startTimer(150));
   this_player.AddState("idle", "./res/game/c1/c1_idel/c1_idel (%%).png", 1, 8);
@@ -78,7 +75,7 @@ void MainWindow::timerEvent(QTimerEvent *event)
 
 void MainWindow::paintEvent(QPaintEvent *event)
 {
-  Player player = this->g.GetPlayer();
+  Player player = *(this->me);
   Point player_location = player.GetLocation();
   int left_top_x = player_location.GetX() - DEFUALT_WIDTH / 2;
   int left_top_y = player_location.GetY() - DEFUALT_HEIGHT / 2;
@@ -106,7 +103,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
     QString fileName = QString("./res/game/decorate%1.png").arg(i);
     pix_decorate[i] = new QPixmap(fileName); // 从文件中加载图像
   }
-  std::map<Point, int> decorates = this->g.GetDecorate(Point(left_top_x, left_top_y), Point(left_top_x + DEFUALT_WIDTH, left_top_y + DEFUALT_HEIGHT));
+  std::map<Point, int> decorates = this->GetDecorate(Point(left_top_x, left_top_y), Point(left_top_x + DEFUALT_WIDTH, left_top_y + DEFUALT_HEIGHT));
   for (auto i = decorates.begin(); i != decorates.end(); i++)
   {
     painter.drawPixmap((*i).first.GetX() - left_top_x, (*i).first.GetY() - left_top_y, DECORATE_SIZE * 2, DECORATE_SIZE * 2, *pix_decorate[(*i).second]);
@@ -197,7 +194,7 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
       pressTimer->stop();
     }
     this->player_animation_list[0].SetState("walk");
-    g.PlayerMove(g.GetPlayer().GetID(), UP);
+    PlayerMove(this->me->GetID(), UP);
   }
   else if (event->key() == Qt::Key_S)
   {
@@ -206,7 +203,7 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
       pressTimer->stop();
     }
     this->player_animation_list[0].SetState("walk");
-    g.PlayerMove(g.GetPlayer().GetID(), DOWN);
+    PlayerMove(this->me->GetID(), DOWN);
   }
   else if (event->key() == Qt::Key_A)
   {
@@ -215,7 +212,7 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
       pressTimer->stop();
     }
     this->player_animation_list[0].SetState("walk");
-    g.PlayerMove(g.GetPlayer().GetID(), LEFT);
+    PlayerMove(this->me->GetID(), LEFT);
   }
   else if (event->key() == Qt::Key_D)
   {
@@ -224,10 +221,11 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
       pressTimer->stop();
     }
     this->player_animation_list[0].SetState("walk");
-    g.PlayerMove(g.GetPlayer().GetID(), RIGHT);
+    PlayerMove(this->me->GetID(), RIGHT);
   }
-  else if (event->key() == Qt::Key_B){
-    this->isbuilding=!this->isbuilding;
+  else if (event->key() == Qt::Key_B)
+  {
+    this->isbuilding = !this->isbuilding;
   }
   else
   {
@@ -238,4 +236,152 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
 void MainWindow::handleDelayedKeyRelease()
 {
   this->player_animation_list[0].SetState("idle");
+}
+
+void MainWindow::Init()
+{
+  // 地图初始化
+
+  // 坐标范围初始化
+  map_x_max = MAP_WIDTH * BLOCK_SIZE;
+  map_y_max = MAP_HEIGHT * BLOCK_SIZE;
+
+  // 装饰品初始化
+  srand(time(0));
+  this->decorates.insert(std::pair<Point, int>(Point(MAP_WIDTH * BLOCK_SIZE / 2, MAP_HEIGHT * BLOCK_SIZE / 2), rand() % 7));
+  int size = rand() % (MAP_WIDTH * MAP_HEIGHT / 4) + MAP_WIDTH;
+  for (int i = 0; i < size; i++)
+  {
+    int x = BLOCK_SIZE + rand() % (BLOCK_SIZE * MAP_WIDTH - 2 * BLOCK_SIZE);
+    int y = BLOCK_SIZE + rand() % (BLOCK_SIZE * MAP_HEIGHT - 2 * BLOCK_SIZE);
+    this->decorates.insert(std::pair<Point, int>(Point(x, y), rand() % 7));
+  }
+}
+
+void MainWindow::SetPlayer(Player *p)
+{
+  if (this->myID == NO_PLAYER)
+  {
+    this->me = p;
+    this->myID = p->GetID();
+    playerList.push_back(p);
+  }
+}
+
+Construct *MainWindow::GetConstruct(int x, int y)
+{
+  if (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT)
+  {
+    return this->gameMap[x][y];
+  }
+  else
+  {
+    return NULL;
+  }
+}
+
+bool MainWindow::AddPlayer(Player *p)
+{
+  // TODO:判断重复
+  this->playerList.push_back(p);
+  return true;
+}
+
+void MainWindow::PlayerMove(int playerID, int direction)
+{
+  for (auto it = playerList.begin(); it != playerList.end(); it++)
+  {
+    if ((*it)->GetID() == playerID)
+    {
+      double speed = (*it)->GetSpeed();
+      if (direction == UP)
+      {
+        Point new_p((*it)->GetLocation().GetX(), (*it)->GetLocation().GetY() - speed);
+        if (new_p.GetY() >= map_y_min && !Block((*it)->GetLocation(), new_p))
+        {
+          (*it)->Move(new_p);
+        }
+      }
+      else if (direction == DOWN)
+      {
+        Point new_p((*it)->GetLocation().GetX(), (*it)->GetLocation().GetY() + speed);
+        if (new_p.GetY() < map_y_max && !Block((*it)->GetLocation(), new_p))
+        {
+          (*it)->Move(new_p);
+        }
+      }
+      else if (direction == LEFT)
+      {
+        Point new_p((*it)->GetLocation().GetX() - speed, (*it)->GetLocation().GetY());
+        if (new_p.GetX() >= map_x_min && !Block((*it)->GetLocation(), new_p))
+        {
+          (*it)->Move(new_p);
+          (*it)->SetTowardsHorizontal(true);
+        }
+      }
+      else if (direction == RIGHT)
+      {
+        Point new_p((*it)->GetLocation().GetX() + speed, (*it)->GetLocation().GetY());
+        if (new_p.GetX() < map_x_max && !Block((*it)->GetLocation(), new_p))
+        {
+          (*it)->Move(new_p);
+          (*it)->SetTowardsHorizontal(false);
+        }
+      }
+      else
+      {
+      }
+    }
+  }
+}
+
+bool MainWindow::Block(Point p1, Point p2)
+{
+  // 目前不能斜向移动 可以先不判定 后续有需求改这
+  if (p1.GetX() != p2.GetX() && p1.GetY() != p2.GetY())
+  {
+    return false;
+  }
+  else if (p1.GetX() != p2.GetX())
+  {
+    int min_b = std::min(p1.GetX(), p2.GetX()) / BLOCK_SIZE;
+    int max_b = std::max(p1.GetX(), p2.GetX()) / BLOCK_SIZE;
+    int y = p1.GetY() / BLOCK_SIZE;
+    for (int i = min_b; i <= max_b; i++)
+    {
+      if (gameMap[i][y] != NULL && gameMap[i][y]->IsBlock())
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  else if (p1.GetY() != p2.GetY())
+  {
+    int min_b = std::min(p1.GetY(), p2.GetY()) / BLOCK_SIZE;
+    int max_b = std::max(p1.GetY(), p2.GetY()) / BLOCK_SIZE;
+    int x = p1.GetX() / BLOCK_SIZE;
+    for (int i = min_b; i <= max_b; i++)
+    {
+      if (gameMap[x][i] != NULL && gameMap[x][i]->IsBlock())
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  return true;
+}
+
+std::map<Point, int> MainWindow::GetDecorate(Point left_top, Point right_down)
+{
+  std::map<Point, int> res;
+  for (auto i = this->decorates.begin(); i != this->decorates.end(); i++)
+  {
+    if ((*i).first > left_top && (*i).first < right_down)
+    {
+      res.insert(*i);
+    }
+  }
+  return res;
 }
--- src/client/mainwindow.h
+++ src/client/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <random>
+#include <list>
+#include <map>
+#include <ctime>
+#include <algorithm>
 
 #include <QMainWindow>
 #include <QPainter>
@@ -13,13 +17,21 @@
 #include <QMouseEvent>
 #include <QTimer>
 
-#include "game.h"
+#include "../core/construct.h"
+#include "../core/player.h"
+#include "../core/animation.h"
 
 #define DEFUALT_WIDTH 960
 #define DEFUALT_HEIGHT 640
 #define BLOCK_SIZE 64
 #define START_PAGE_BUTTON_WIDTH DEFUALT_WIDTH / 6
 #define DECORATE_SIZE 32
+#define MAP_WIDTH 256
+#define MAP_HEIGHT 256
+#define UP 1
+#define RIGHT 2
+#define DOWN 3
+#define LEFT 4
 
 namespace Ui
 {
@@ -39,15 +51,28 @@ public:
   void keyReleaseEvent(QKeyEvent *event);
   void handleDelayedKeyRelease();
 
+  void Init();
+  std::map<Point, int> GetDecorate(Point left_top, Point right_down);
+  void SetPlayer(Player *p);
+  bool AddPlayer(Player *p);
+  Construct *GetConstruct(int x, int y);
+  void PlayerMove(int playerID, int direction);
+  bool Block(Point p1, Point p2);
+
 private:
   Ui::MainWindow *ui;
-  Game g;
+  Construct *gameMap[MAP_WIDTH][MAP_HEIGHT];
+  std::list<Player *> playerList;
+  int myID = NO_PLAYER;
+  Player *me = NULL;
+  std::map<Point, int> decorates;
+  int map_x_min = 0, map_y_min = 0, map_x_max = 0, map_y_max = 0;
   int REFRESH;
   bool is_playing = 0;
   QMediaPlayer *mediaPlayer;
   QMediaPlaylist *playlist;
   QPoint mousePosition;
-  bool isbuilding = 1;
+  bool isbuilding = false;
   int current_build_cursor_frame = 0;
   int ANIMATION_TIME;
   Animation building_cursor;
